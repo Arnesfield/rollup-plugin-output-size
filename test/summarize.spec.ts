fe02b84317cb313c8c18123d7b8a73c4b5a7ea97
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-12-23 18:47:12 +0800

    Add summarize spec

---
+++ test/summarize.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import stripAnsi from 'strip-ansi';
+import { summarize, Summary, SummarySizes } from '../src';
+
+describe('summarize', () => {
+  it('should be a function', () => {
+    expect(summarize).to.be.a('function');
+  });
+
+  it('should return a string', () => {
+    const sizes: SummarySizes = {
+      asset: { size: 20, hSize: '20 B' },
+      chunk: { size: 30, hSize: '30 B' },
+      entry: { size: 40, hSize: '40 B' },
+      total: { size: 90, hSize: '90 B' }
+    };
+    const summary: Summary = { ...sizes, gzip: sizes };
+    const formatted = summarize(summary);
+    expect(formatted).to.be.a('string');
+    const raw = stripAnsi(formatted);
+    const text = '[total] 40 B + 30 B + 20 B = 90 B';
+    expect(raw).to.equal(`${text}\n${text} (gzip)`);
+  });
+
+  it('should handle no gzip info', () => {
+    const summary: Summary = {
+      asset: { size: 20, hSize: '20 B' },
+      chunk: { size: 30, hSize: '30 B' },
+      entry: { size: 40, hSize: '40 B' },
+      total: { size: 90, hSize: '90 B' }
+    };
+    const formatted = summarize(summary);
+    expect(formatted).to.be.a('string');
+    const raw = stripAnsi(formatted);
+    expect(raw).to.equal('[total] 40 B + 30 B + 20 B = 90 B');
+  });
+});
