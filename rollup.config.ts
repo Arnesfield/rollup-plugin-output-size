0d1922af3b027d71ae78ceb3d68ef6f814203198
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-04-11 14:41:53 +0800

    Use cleanup plugin to strip comments from cjs build Use edit plugin to remove unused requires from gzip-size

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,9 @@ import _eslint from '@rollup/plugin-eslint';
 import _nodeResolve from '@rollup/plugin-node-resolve';
 import _typescript from '@rollup/plugin-typescript';
 import { RollupOptions } from 'rollup';
+import cleanup from 'rollup-plugin-cleanup';
 import dts from 'rollup-plugin-dts';
+import edit from 'rollup-plugin-edit';
 import esbuild from 'rollup-plugin-esbuild';
 import pkg from './package.json' with { type: 'json' };
 import outputSize from './src/index.js';
@@ -19,11 +21,18 @@ const WATCH = process.env.ROLLUP_WATCH === 'true';
 const external = Object.keys(pkg.dependencies).concat('path');
 const input = 'src/index.ts';
 const esmExternals = ['gzip-size', 'pretty-bytes'];
-// disable sourcemaps (enable for preview)
-const sourcemap = false;
+// disable sourcemaps (enable only for preview)
+const preview = false;
 
 function build() {
   return esbuild({ target: 'esnext' });
+}
+
+function clean() {
+  return cleanup({
+    comments: ['some', 'sources', /__PURE__/],
+    extensions: ['js', 'ts']
+  });
 }
 
 function defineConfig(options: (false | RollupOptions)[]) {
@@ -34,8 +43,8 @@ export default defineConfig([
   {
     input,
     external,
-    output: { file: pkg.module, format: 'esm', sourcemap },
-    plugins: [build(), outputSize()]
+    output: { file: pkg.module, format: 'esm', sourcemap: preview },
+    plugins: [build(), clean(), outputSize()]
   },
   {
     input,
@@ -44,12 +53,39 @@ export default defineConfig([
       dir: 'lib',
       format: 'cjs',
       exports: 'named',
-      sourcemap,
+      sourcemap: preview,
       entryFileNames: '[name].cjs',
       chunkFileNames: '[name].cjs',
       manualChunks: { vendor: esmExternals }
     },
-    plugins: [build(), commonjs(), nodeResolve(), outputSize()]
+    plugins: [
+      build(),
+      clean(),
+      commonjs(),
+      nodeResolve(),
+      // remove unused requires from gzip-size
+      edit({
+        chunk(data) {
+          const pkgs = ['stream', 'node:fs', 'node:stream'];
+          let edited = false;
+          let { contents } = data;
+          for (const pkg of pkgs) {
+            const match = `require('${pkg}');\n`;
+            if (!contents.includes(match)) {
+              continue;
+            } else if (!preview) {
+              console.log('[edit] %s: removing require %o', data.fileName, pkg);
+            }
+            edited = true;
+            contents = contents.replace(match, '');
+          }
+          if (edited) {
+            return contents;
+          }
+        }
+      }),
+      outputSize()
+    ]
   },
   {
     input,
