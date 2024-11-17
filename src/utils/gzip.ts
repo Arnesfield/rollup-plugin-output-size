70cfa12bd6ab1ab2eb455a50eedc7d60287ae358
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-11-17 18:16:58 +0800

    Update gzip util, remove gzip-size usage

--- src/utils/gzip.ts
+++ src/utils/gzip.ts
@@ -1,6 +1,9 @@
-import { gzipSize } from 'gzip-size';
 import prettyBytes from 'pretty-bytes';
+import { promisify } from 'util';
+import { gzip as __gzip } from 'zlib';
 import { Size } from '../types/size.types.js';
+
+const _gzip = promisify(__gzip);
 
 /**
  * Get the gzipped size of input.
@@ -8,8 +11,6 @@ import { Size } from '../types/size.types.js';
  * @returns The gzipped size.
  */
 export async function gzip(input: string | Uint8Array): Promise<Size> {
-  const size = await gzipSize(
-    typeof input === 'string' ? input : Buffer.from(input)
-  );
+  const size = input ? (await _gzip(input, { level: 9 })).byteLength : 0;
   return { size, hSize: prettyBytes(size) };
 }
