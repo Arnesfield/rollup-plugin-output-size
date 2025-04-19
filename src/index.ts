e53e6f18ab7670108b7e43cb8c04a3cc208a34d6
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-19 15:06:37 +0800

    Remove subdirectory barrel files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export * from './core';
-export { outputSize as default } from './core';
-export * from './types';
-export * from './utils';
+export * from './core/output-size';
+export { outputSize as default } from './core/output-size';
+export * from './types/core.types';
+export * from './types/output.types';
+export * from './types/size.types';
+export * from './types/summary.types';
+export * from './utils/format';
+export * from './utils/gzip';
+export * from './utils/summarize';
--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './core.types';
-export * from './output.types';
-export * from './size.types';
-export * from './summary.types';
