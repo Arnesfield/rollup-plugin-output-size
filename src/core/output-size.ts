d6169b6e138da3ef2f7542371c001f4b3b06624b
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-05-30 22:50:09 +0800

    Refactor plugin and rename some variables

--- src/core/output-size.ts
+++ src/core/output-size.ts
@@ -4,11 +4,16 @@ import { KEYS } from '../constants';
 import { Options } from '../types/core.types';
 import { OutputInfo } from '../types/output.types';
 import { Size } from '../types/size.types';
-import { Summary, SummaryOutput } from '../types/summary.types';
+import { SummaryOutput, SummarySizes } from '../types/summary.types';
 import { format } from '../utils/format';
 import { formatBytes } from '../utils/format-bytes';
-import { gzip as getGzip } from '../utils/gzip';
+import { gzip } from '../utils/gzip';
 import { summarize } from '../utils/summarize';
+
+// same as Summary but props are required
+interface PluginSummary extends Required<SummarySizes> {
+  gzip?: Required<SummarySizes>;
+}
 
 /**
  * A Rollup plugin that displays output bundle sizes.
@@ -22,14 +27,13 @@ export function outputSize(options: Options = {}): Plugin {
     : options.gzip == null || !!options.gzip || [];
   const summaryOpts = options.summary == null || options.summary;
 
-  let summaries: SummaryOutput[];
-  let didSummarize: boolean;
+  let summaries: SummaryOutput[], summarized: boolean;
 
   return {
     name: 'output-size',
     outputOptions() {
       summaries = [];
-      didSummarize = false;
+      summarized = false;
     },
     async generateBundle(outputOpts, bundle) {
       if (options.silent) return;
@@ -37,26 +41,26 @@ export function outputSize(options: Options = {}): Plugin {
       for (const output of Object.values(bundle)) {
         const isChunk = output.type === 'chunk';
         const type = isChunk && output.isEntry ? 'entry' : output.type;
-        const shouldHide = hide === true || hide.includes(type);
+        const hidden = hide === true || hide.includes(type);
         // skip other checks if no summary is required
-        if (!summaryOpts && shouldHide) continue;
+        if (!summaryOpts && hidden) continue;
 
         const data = isChunk ? output.code : output.source;
         const size =
           typeof data === 'string' ? Buffer.byteLength(data) : data.byteLength;
         const hSize = formatBytes(size);
-        const gzip =
-          gzipOpts === true || gzipOpts.includes(type)
-            ? await getGzip(data)
-            : undefined;
         const filePath = path.join(
           outputOpts.dir || path.dirname(outputOpts.file || ''),
           output.fileName
         );
-        const info: OutputInfo = { path: filePath, type, size, hSize, gzip };
+        const info: OutputInfo = { path: filePath, type, size, hSize };
+        if (gzipOpts === true || gzipOpts.includes(type)) {
+          info.gzip = await gzip(data);
+        }
+
         summaries.push({ info, output });
 
-        if (shouldHide) {
+        if (hidden) {
           // do nothing
         } else if (typeof options.handle === 'function') {
           await options.handle(info, output);
@@ -66,11 +70,11 @@ export function outputSize(options: Options = {}): Plugin {
       }
     },
     async writeBundle() {
-      if (didSummarize || !summaryOpts || options.silent) return;
-      didSummarize = true;
+      if (summarized || !summaryOpts || options.silent) return;
+      summarized = true;
 
       // create summary
-      const summary = { gzip: {} } as Summary;
+      const summary = { gzip: {} } as PluginSummary;
       for (const key of KEYS) {
         summary[key] = { size: 0, hSize: '0 B' };
         summary.gzip![key] = { size: 0, hSize: '0 B' };
@@ -79,12 +83,12 @@ export function outputSize(options: Options = {}): Plugin {
       // set summary sizes
       for (const { info } of summaries) {
         summary.total.size += info.size;
-        summary[info.type]!.size += info.size;
+        summary[info.type].size += info.size;
         if (!summary.gzip) {
           // exclude gzip if an info does not have gzip size
         } else if (info.gzip) {
           summary.gzip.total.size += info.gzip.size;
-          summary.gzip[info.type]!.size += info.gzip.size;
+          summary.gzip[info.type].size += info.gzip.size;
         } else {
           delete summary.gzip;
         }
@@ -93,14 +97,14 @@ export function outputSize(options: Options = {}): Plugin {
       // update hSizes
       const s = (size: Size) => (size.hSize = formatBytes(size.size));
       for (const key of KEYS) {
-        s(summary[key]!);
-        summary.gzip && s(summary.gzip[key]!);
+        s(summary[key]);
+        summary.gzip && s(summary.gzip[key]);
       }
 
       // display summary (preserve `this` for callbacks)
       if (typeof options.summary === 'function') {
         await options.summary(summary, summaries);
-      } else if (summaryOpts === 'always' || summaries.length > 1) {
+      } else if (summaries.length > 1 || summaryOpts === 'always') {
         // by default, show summary only if more than 1 output
         console.log(summarize(summary, options));
       }
