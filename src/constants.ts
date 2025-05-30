c7a69e1f9ca16b09649a3c5d8ce7b42e82ac2c37
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-05-30 15:49:31 +0800

    Use `util.styleText()` for log colors

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,8 @@
-import { blue, cyan, magenta } from 'colorette';
-
 // NOTE: internal
 
 export const OUTPUT_TYPES = ['entry', 'chunk', 'asset'] as const;
 
 export const TYPES = [...OUTPUT_TYPES, 'total'] as const;
 
-export const COLOR = { asset: magenta, chunk: blue, entry: cyan } as const;
+// prettier-ignore
+export const COLOR = { asset: 'magenta', chunk: 'blue', entry: 'cyan' } as const;
